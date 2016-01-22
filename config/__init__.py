b514458446859f5ba36b8e9d805fc6a6a78f5e96
billvsme <994171686@qq.com>
2016-01-22 17:07:45 +0800

    Add python version judge

--- config/__init__.py
+++ config/__init__.py
@@ -1,12 +1,18 @@
-import configparser
 import os
+import sys
+
+if sys.version_info < (3, 0):
+    import ConfigParser
+    config = ConfigParser.ConfigParser()
+else:
+    import configparser
+    config = configparser.ConfigParser()
 
 
 config_dev_path = './{}/config.ini'.format(__name__)
 config_path = './{}/config_dev.ini'.format(__name__)
 
 # read config.ini file
-config = configparser.ConfigParser()
 if os.path.exists(config_path):
     config.read(config_path)
 else:
