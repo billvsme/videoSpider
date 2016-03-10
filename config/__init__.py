a01f40a162c074c63a696fafac2ba34d6cab217a
billvsme <994171686@qq.com>
2016-03-10 23:03:58 +0800

    Add .travis.yml

--- config/__init__.py
+++ config/__init__.py
@@ -4,13 +4,15 @@ import sys
 from celery import Celery
 from whoosh import index
 from whoosh.fields import *
-from jieba.analyse import ChineseAnalyzer; analyzer = ChineseAnalyzer()
+from jieba.analyse import ChineseAnalyzer
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from celery.signals import worker_process_init
 from sqlalchemy import event
 from sqlalchemy import exc
 from models import Base
+
+analyzer = ChineseAnalyzer()
 
 if sys.version_info < (3, 0):
     import ConfigParser
@@ -43,11 +45,9 @@ engine = create_new_engine()
 Session = sessionmaker(bind=engine)
 session = Session()
 
-#Base.metadata.create_all(engine)
-
 
 '''Why use a dict save engine and session?
-Because using multiprocess, you should create new connection to database. 
+Because using multiprocess, you should create new connection to database.
 In the begin, I see the
 http://docs.sqlalchemy.org/en/latest/core/pooling.html#using-connection-pools-with-multiprocessing,
 and use above approaches, but on the new process start, have some error.
@@ -56,6 +56,7 @@ sqla = {
     'engine': engine,
     'session': session
 }
+
 
 @worker_process_init.connect
 def new_process(signal, sender):
@@ -88,12 +89,15 @@ celebrity_schema = Schema(
 
 exists = index.exists_in("indexdir")
 
-if exists == True:
+if exists:
     video_ix = index.open_dir("indexdir", indexname='video')
     celebrity_ix = index.open_dir("indexdir", indexname='celebrity')
 else:
     if not os.path.exists("indexdir"):
         os.mkdir("indexdir")
-    video_ix = index.create_in("indexdir", schema=video_schema, indexname='video')
-    celebrity_ix = index.create_in("indexdir", schema=celebrity_schema, indexname='celebrity')
-
+    video_ix = index.create_in("indexdir",
+                               schema=video_schema,
+                               indexname='video')
+    celebrity_ix = index.create_in("indexdir",
+                                   schema=celebrity_schema,
+                                   indexname='celebrity')
