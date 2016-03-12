42a6b8a5c547cf678900bd1f9d4852361edfe75b
billvsme <994171686@qq.com>
2016-03-12 23:28:51 +0800

    Update tests

--- tests/test_start.py
+++ tests/test_start.py
@@ -1,2 +1,20 @@
-def test_start():
-    pass
+import pytest
+import webs
+
+
+class TestVideoSpider():
+    def test_entry_points(self):
+        webs.douban.parsers.movie
+        webs.douban.parsers.celebrity
+        webs.douban.parsers.douban_api
+        webs.douban.parsers.movie_photo
+        webs.douban.tasks.get_main_movies_base_data
+        webs.douban.tasks.get_main_movies_full_data
+        webs.douban.tasks.get_celebrities_full_data
+        webs.douban.tasks.down_video_images
+        webs.douban.tasks.down_celebrity_images
+
+        webs.bilibili.parsers.bilibili_api
+        webs.bilibili.parsers.animation
+        webs.bilibili.tasks.get_animations_base_data
+        webs.bilibili.tasks.get_animations_full_data
