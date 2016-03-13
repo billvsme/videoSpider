4e60f220bae372cd81d0ea217e252b74679c4ff7
billvsme <994171686@qq.com>
2016-03-13 15:09:47 +0800

    Add some douban_movie tests

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base
+from config import create_new_sqla
+from helpers import get_video_douban_ids
+
+test_database_url = 'sqlite:///test.db'
+
+
+@pytest.fixture(scope='session')
+def session(request):
+    sqla = create_new_sqla(test_database_url, echo=False)
+
+    session = sqla['session']
+    engine = sqla['engine']
+
+    Base.metadata.create_all(engine)
+
+    def teardown():
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(teardown)
+
+    return session
+
+@pytest.fixture
+def douban_movie_ids():
+    return list(get_video_douban_ids())
--- tests/test_start.py
+++ tests/test_start.py
@@ -1,5 +1,8 @@
 import pytest
 import webs
+import models
+from gevent import monkey
+monkey.patch_socket()
 
 
 class TestVideoSpider():
@@ -18,3 +21,25 @@ class TestVideoSpider():
         webs.bilibili.parsers.animation
         webs.bilibili.tasks.get_animations_base_data
         webs.bilibili.tasks.get_animations_full_data
+
+    def test_douban_movie_base_data(self, session):
+        douban_movie_ids = webs.douban.tasks.get_main_movies_base_data.task(
+            20,
+            types=['movie'],
+            sorts=['recommend'],
+            tags_dict={
+                'movie': ['热门']
+            }
+        )
+
+        self.douban_movie_ids = douban_movie_ids
+
+    def test_douban_movie_full_data(self, session, douban_movie_ids):
+        webs.douban.tasks.get_main_movies_full_data.task(
+            douban_movie_ids[:10],
+            5
+        )
+
+        videos = session.query(models.Video).filter_by(is_detail=True).all()
+
+        assert len(videos) == 10
